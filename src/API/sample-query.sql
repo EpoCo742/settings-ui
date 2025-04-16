923e065152d9eea736ba3a971a3b22ba5ee2509d
Evan <evan.ovalle@gmail.com>
2025-04-16 09:37:58 -0600

    Update sample-query.sql

--- src/API/sample-query.sql
+++ src/API/sample-query.sql
@@ -10,26 +10,25 @@ SELECT
   s.is_deleted,
   s.deleted_by,
   s.deleted_at,
-  -- Aggregate associated arrays
-  (
-    SELECT LISTAGG(t.value, ',') WITHIN GROUP (ORDER BY t.value)
-    FROM transaction_types t
-    WHERE t.settings_id = s.settings_id
-  ) AS transaction_types,
-  (
-    SELECT LISTAGG(f.value, ',') WITHIN GROUP (ORDER BY f.value)
-    FROM frequency f
-    WHERE f.settings_id = s.settings_id
-  ) AS frequency,
-  (
-    SELECT LISTAGG(i.value, ',') WITHIN GROUP (ORDER BY i.value)
-    FROM indicator i
-    WHERE i.settings_id = s.settings_id
-  ) AS indicator,
-  (
-    SELECT LISTAGG(a.value, ',') WITHIN GROUP (ORDER BY a.value)
-    FROM authorization a
-    WHERE a.settings_id = s.settings_id
-  ) AS authorization
+  LISTAGG(t.value, ',') WITHIN GROUP (ORDER BY t.value) AS transaction_types,
+  LISTAGG(f.value, ',') WITHIN GROUP (ORDER BY f.value) AS frequency,
+  LISTAGG(i.value, ',') WITHIN GROUP (ORDER BY i.value) AS indicator,
+  LISTAGG(a.value, ',') WITHIN GROUP (ORDER BY a.value) AS authorization
 FROM settings s
-WHERE s.settings_id = :settingId;
+LEFT JOIN transaction_types t ON t.settings_id = s.settings_id
+LEFT JOIN frequency f ON f.settings_id = s.settings_id
+LEFT JOIN indicator i ON i.settings_id = s.settings_id
+LEFT JOIN authorization a ON a.settings_id = s.settings_id
+WHERE s.settings_id = :settingId
+GROUP BY
+  s.settings_id,
+  s.group_id,
+  s.group_name,
+  s.created_by,
+  s.updated_by,
+  s.date_created,
+  s.date_updated,
+  s.threshold,
+  s.is_deleted,
+  s.deleted_by,
+  s.deleted_at;
